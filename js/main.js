100bdde218d4dd869aabbb8af56cda7591550961
Thomas Gossart <tg.59290@gmail.com>
2016-07-31 20:58:58 +0200

    added hover animation effect in main.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@ $("#h2_landing").hide();
 $("#h3_landing").hide();
 $("#h2_landing").show();
 $("#h3_landing").show('slow');
+
+// Set an animation on prestation page
+
+// hide the packages at the beginning
+$(".package").hide();
+
+// Allows the packages to display in cascade
+  $("#beginner").fadeIn('slow', function(){
+    $("#regular").fadeIn('slow', function nextone (){
+      $("#gold").fadeIn('slow');
+    });
+  });
+
+// hover effect when mouse over fa icons
+  $("#beginner, #regular, #gold").mouseenter(function() {
+      for (var i=1; i<=3; i++) {
+      $(this).animate({bottom: 20},"slow");
+      $(this).animate({bottom: 0},"slow");
+           }
+  });
+
+  $("#beginner, #regular, #gold").mouseleave(function() {
+    $(this).stop();
+  });
