3b982a2d422b928ec37495285e8dbb9c234a28be
Thomas Gossart <tg.59290@gmail.com>
2016-08-02 11:09:48 +0200

    added dialogue boxes on click on reserve and contact button in main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+/*
+
+~~~~~~~Landing page~~~~~~~~~
+
+*/
+
 // allow the apear effect for h2 and h3 on the landing page
 $("#h2_landing").hide();
 $("#h3_landing").hide();
 $("#h2_landing").show();
 $("#h3_landing").show('slow');
+
+/*
+
+~~~~~~~Prestation page~~~~~~~~~
+
+*/
 
 // Set an animation on prestation page
 
@@ -24,7 +36,40 @@ $(".package").hide();
            }
   });
 
+// Stop the animation when mousse leaves
   $("#beginner, #regular, #gold").mouseleave(function() {
     $(this).stop(true);
     $(this).animate({bottom: 0},"slow");
+  });
+
+
+/*
+
+~~~~~~~ Calendrier page~~~~~~~~~
+
+*/
+
+// Open prompt dialoge box when click on réserver button
+var answer = "";
+var up = "";
+
+  $("#reserve_button").click(function() {
+    answer = prompt("Êtes-vous sûr de vouloir réserver");
+    up = answer.toUpperCase();
+    if (up == "OUI") {
+      alert ("Parfait votre séance est réservée");
+    }
+    else {
+      alert ("D'accord prenez un peu plus de temps pour réfléchir");
+    }
+  });
+
+  /*
+
+  ~~~~~~~Contact page~~~~~~~~~
+
+  */
+
+  $("#contact_button").click(function(){
+    alert ("Votre message a bien été envoyé, je vous répondrai dans les plus brefs délais");
   });
